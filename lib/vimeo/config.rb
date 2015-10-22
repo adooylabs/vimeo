8f3505d24fdb1f0adcf344d058a502a000146ec6
Adooylabs <amvelasco@gmail.com>
2015-10-22 11:39:23 -0700

    adding configure block

---
+++ lib/vimeo/config.rb
@@ -0,0 +1,14 @@
+module Vimeo
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :consumer_key, :consumer_secret
+  end
+end
